f3c980abc21973cacc36d9c43e3737fec83b4926
Andrew Nguyen <nguyenandrew137@protonmail.com>
2025-05-23 17:50:01 -0400

    feat: threading to not hangup on slow clients
    Performance Notes:
    The test I used was: `ab -n 100000 -c 2048 http://127.0.0.1:8080/pictures/smudge.jpg`
    size(smudge.jpg) = 766420 bytes
    Transfer rate before change: ~3.2gb/s
    Transfer rate after change: ~4gb/s

--- waiter.c
+++ waiter.c
@@ -7,6 +7,8 @@
 #include <fcntl.h>
 #include <glob.h>
 #include <linux/limits.h>
+#include <pthread.h>
+#include <semaphore.h>
 #include <string.h>
 #include <sys/mman.h>
 #include <sys/sendfile.h>
@@ -25,8 +27,17 @@
 // constants
 #define RECV_BUFFER_CAP 8192
 #define SEND_BUFFER_CAP 8192
+#define THREAD_COUNT 16
+
+// structs
+struct thread_data
+{
+    int client;
+    sem_t notify;
+};
 
 // globals
+char _curr_dir[PATH_MAX] = {0};
 int _server_fd = 0;
 char _file_index[] = "index.html";
 char _file_error_404[] = "404.html";
@@ -38,6 +49,7 @@ char *_mime_types[][2] = {
 char *_blacklisted_config[] = {
     ".git/",
 };
+glob_t _blacklisted = {0};
 
 // functions
 char const *
@@ -165,6 +177,179 @@ user_handle_url(
     return(0);
 }
 
+void *
+handle_connection(void *data_ptr)
+{
+    struct thread_data *data = data_ptr;
+    while(1)
+    {
+        sem_wait(&data->notify);
+
+        char recv_buf[RECV_BUFFER_CAP] = {0};
+        ssize_t recv_buf_size = 0;
+        recv_buf_size = recv(data->client, recv_buf, RECV_BUFFER_CAP, 0); // TODO: unfinished reads
+        if(recv_buf_size == 0) {goto EXIT_REQUEST;}
+        if(recv_buf_size == RECV_BUFFER_CAP) // NOTE: we do not support dynamic size request
+        {
+            do // 'finish' read
+            {
+                recv_buf_size = recv(data->client, recv_buf, RECV_BUFFER_CAP, 0);
+                if(recv_buf_size == 0) {goto EXIT_REQUEST;}
+            } while(recv_buf_size == RECV_BUFFER_CAP);
+            send_request_denied(data->client);
+            goto EXIT_REQUEST;
+        }
+        if(recv_buf_size < 0) {diep("recv");}
+        char *recv_buf_end = recv_buf + recv_buf_size;
+
+        // recv parse
+        char *method = recv_buf;
+        char *url = NULL;
+        char *params = NULL;
+        char *protocol = NULL;
+        char *rest = NULL;
+
+        for(url = method; url < recv_buf_end; url++)
+        {
+            if(*url == ' ') {*(url++) = '\0'; break;}
+        }
+        if(url == recv_buf_end) {send_request_denied(data->client); goto EXIT_REQUEST;}
+
+        params = url;
+        for(protocol = url; protocol < recv_buf_end; protocol++)
+        {
+            if(*protocol == '?') {*(protocol++) = '\0'; params = protocol;}
+            if(*protocol == ' ') {*(protocol++) = '\0'; break;}
+        }
+        if(params == recv_buf_end) {send_request_denied(data->client); goto EXIT_REQUEST;}
+
+        for(rest = protocol; rest < recv_buf_end; rest++)
+        {
+            if(isspace(*rest)) {*(rest++) = '\0'; break;}
+        }
+        if(rest == recv_buf_end) {send_request_denied(data->client); goto EXIT_REQUEST;}
+
+        // verify request parameters
+        if(strcmp(method, "GET") != 0) {log("failed: was not GET\n"); goto EXIT_REQUEST;}
+        if(strcmp(protocol, "HTTP/1.0") != 0
+                && strcmp(protocol, "HTTP/1.1") != 0)
+        {
+            log("failed: http protocol was not supported: %s\n", protocol);
+            goto EXIT_REQUEST;
+        }
+        log(">>> URL: %s\n", url);
+        if(params != url) {log("PARAMS: %s\n", params);}
+
+        /* handle request */
+        size_t url_size = strlen(url);
+        if(user_handle_url(data->client, url, url_size))
+        {
+            goto EXIT_REQUEST;
+        }
+
+        // strip leading slash
+        url_size--;
+        url++;
+
+        /* url verification */
+        char temp_filename[PATH_MAX] = {0};
+        // index handling
+        if(url_size == 0) {url = _file_index;}
+        else if(url[url_size - 1] == '/')
+        {
+            memcpy(temp_filename, url, url_size);
+            memcpy(temp_filename + url_size, _file_index, sizeof(_file_index)); // NOTE: has \0 at the end
+            url = temp_filename;
+        }
+        else if(!file_is_reg(url))
+        {
+            memcpy(temp_filename, url, url_size);
+            temp_filename[url_size] = '/';
+            memcpy(temp_filename + url_size + 1, _file_index, sizeof(_file_index)); // NOTE: has \0 at the end
+            url = temp_filename;
+        }
+        else
+        {
+            // blacklisted prefixes
+            uint8_t valid = 1;
+            for(size_t i = 0;
+                    i < _blacklisted.gl_pathc;
+                    i++)
+            {
+                if(strstr(url, _blacklisted.gl_pathv[i]) == url)
+                {
+                    url = _file_error_404;
+                    valid = 0;
+                }
+            }
+
+            // prevent parent directory traversal
+            if(valid)
+            {
+                realpath(url, temp_filename);
+                if(memcmp(_curr_dir, temp_filename, strlen(_curr_dir)) != 0)
+                {
+                    url = _file_error_404;
+                }
+                else
+                {
+                    url = temp_filename;
+                }
+            }
+        }
+        url_size = strlen(url);
+        log("NEW URL: %s\n", url);
+
+        /* find file */
+        int fd = open(url, O_RDONLY);
+        size_t file_size = 0;
+
+        char *header_type = "HTTP/1.1 200 OK";
+        // check 404
+        if(fd < 0)
+        {
+            if(url == _file_error_404) {send_default_404_msg(data->client); goto EXIT_REQUEST;}
+
+            url = _file_error_404;
+            fd = open(url, O_RDONLY);
+            if(fd < 0) {send_default_404_msg(data->client); goto EXIT_REQUEST;}
+
+            file_size = file_get_size(url);
+            header_type = "HTTP/1.1 404 NOT FOUND";
+        }
+
+        /* send */
+        // send header
+        char send_buf[SEND_BUFFER_CAP] = {0};
+        size_t send_buf_offset = 0;
+        file_size = file_get_size(url);
+        send_buf_offset = snprintf(
+                send_buf, SEND_BUFFER_CAP,
+                "%s\r\n"
+                "Content-Type: %s\r\n"
+                "Content-Length: %ld\r\n"
+                "\r\n",
+                header_type,
+                mime_type_default(url),
+                file_size);
+        if(send_buf_offset > SEND_BUFFER_CAP) {die("failed to write header");}
+        socket_send_all(data->client, send_buf, send_buf_offset);
+
+        // send file
+        if(sendfile(data->client, fd, NULL, file_size) < 0)
+        {
+            diep("failed to send file");
+        }
+        close(fd);
+
+EXIT_REQUEST:
+        close(data->client);
+        data->client = -1;
+    }
+
+    return(NULL);
+}
+
 int
 main(void)
 {
@@ -211,27 +396,47 @@ main(void)
 
     // we only serve from the public directory
     chdir("public/");
-    char curr_dir[PATH_MAX] = {0};
-    if(getcwd(curr_dir, PATH_MAX) == NULL)
+    if(getcwd(_curr_dir, PATH_MAX) == NULL)
     {
         diep("failed to get current directory");
     }
 
     // setup blacklist paths
-    glob_t blacklisted = {0};
     for(size_t i = 0;
             i < static_array_size(_blacklisted_config);
             i++)
     {
         int flags = GLOB_MARK | GLOB_NOSORT;
         if(i != 0) {flags |= GLOB_APPEND;}
-        glob(_blacklisted_config[i], flags, NULL, &blacklisted);
+        glob(_blacklisted_config[i], flags, NULL, &_blacklisted);
+    }
+
+    // spawn threads
+    pthread_t thread_ids[THREAD_COUNT] = {0};
+    struct thread_data thread_data_arr[THREAD_COUNT] = {0};
+    for(size_t i = 0; i < THREAD_COUNT; i++)
+    {
+        thread_data_arr[i].client = -1;
+        if(sem_init(&thread_data_arr[i].notify, 0, 0) != 0)
+        {
+            die("failed to init notify semaphore");
+        }
+
+        if(pthread_create(
+                &thread_ids[i],
+                NULL,
+                handle_connection,
+                &thread_data_arr[i]
+                ) != 0)
+        {
+            diep("failed to spawn thread");
+        }
     }
 
     // accept loop
+    size_t thread_to_add_to = 0;
     while(1)
     {
-        char recv_buf[RECV_BUFFER_CAP] = {0};
         struct sockaddr_in client_addr = {0};
         socklen_t client_addr_size = sizeof(client_addr);
         int client_fd = 0;
@@ -245,167 +450,16 @@ main(void)
             continue;
         }
 
-        ssize_t recv_buf_size = 0;
-        recv_buf_size = recv(client_fd, recv_buf, RECV_BUFFER_CAP, 0); // TODO: unfinished reads
-        if(recv_buf_size == 0) {goto EXIT_REQUEST;}
-        if(recv_buf_size == RECV_BUFFER_CAP) // NOTE: we do not support dynamic size request
+        // add to thread client
+        while(thread_data_arr[thread_to_add_to].client != -1)
         {
-            do // 'finish' read
-            {
-                recv_buf_size = recv(client_fd, recv_buf, RECV_BUFFER_CAP, 0);
-                if(recv_buf_size == 0) {goto EXIT_REQUEST;}
-            } while(recv_buf_size == RECV_BUFFER_CAP);
-            send_request_denied(client_fd);
-            goto EXIT_REQUEST;
+            thread_to_add_to = (thread_to_add_to + 1) % THREAD_COUNT;
         }
-        if(recv_buf_size < 0) {diep("recv");}
-        char *recv_buf_end = recv_buf + recv_buf_size;
-
-        // recv parse
-        char *method = recv_buf;
-        char *url = NULL;
-        char *params = NULL;
-        char *protocol = NULL;
-        char *rest = NULL;
-
-        for(url = method; url < recv_buf_end; url++)
-        {
-            if(*url == ' ') {*(url++) = '\0'; break;}
-        }
-        if(url == recv_buf_end) {send_request_denied(client_fd); goto EXIT_REQUEST;}
-
-        params = url;
-        for(protocol = url; protocol < recv_buf_end; protocol++)
-        {
-            if(*protocol == '?') {*(protocol++) = '\0'; params = protocol;}
-            if(*protocol == ' ') {*(protocol++) = '\0'; break;}
-        }
-        if(params == recv_buf_end) {send_request_denied(client_fd); goto EXIT_REQUEST;}
-
-        for(rest = protocol; rest < recv_buf_end; rest++)
-        {
-            if(isspace(*rest)) {*(rest++) = '\0'; break;}
-        }
-        if(rest == recv_buf_end) {send_request_denied(client_fd); goto EXIT_REQUEST;}
-
-        // verify request parameters
-        if(strcmp(method, "GET") != 0) {log("failed: was not GET\n"); goto EXIT_REQUEST;}
-        if(strcmp(protocol, "HTTP/1.0") != 0
-                && strcmp(protocol, "HTTP/1.1") != 0)
-        {
-            log("failed: http protocol was not supported: %s\n", protocol);
-            goto EXIT_REQUEST;
-        }
-        log(">>> URL: %s\n", url);
-        if(params != url) {log("PARAMS: %s\n", params);}
-
-        /* handle request */
-        size_t url_size = strlen(url);
-        if(user_handle_url(client_fd, url, url_size))
-        {
-            goto EXIT_REQUEST;
-        }
-
-        // strip leading slash
-        url_size--;
-        url++;
-
-        /* url verification */
-        char temp_filename[PATH_MAX] = {0};
-        // index handling
-        if(url_size == 0) {url = _file_index;}
-        else if(url[url_size - 1] == '/')
-        {
-            memcpy(temp_filename, url, url_size);
-            memcpy(temp_filename + url_size, _file_index, sizeof(_file_index)); // NOTE: has \0 at the end
-            url = temp_filename;
-        }
-        else if(!file_is_reg(url))
-        {
-            memcpy(temp_filename, url, url_size);
-            temp_filename[url_size] = '/';
-            memcpy(temp_filename + url_size + 1, _file_index, sizeof(_file_index)); // NOTE: has \0 at the end
-            url = temp_filename;
-        }
-        else
-        {
-            // blacklisted prefixes
-            uint8_t valid = 1;
-            for(size_t i = 0;
-                    i < blacklisted.gl_pathc;
-                    i++)
-            {
-                if(strstr(url, blacklisted.gl_pathv[i]) == url)
-                {
-                    url = _file_error_404;
-                    valid = 0;
-                }
-            }
-
-            // prevent parent directory traversal
-            if(valid)
-            {
-                realpath(url, temp_filename);
-                if(memcmp(curr_dir, temp_filename, strlen(curr_dir)) != 0)
-                {
-                    url = _file_error_404;
-                }
-                else
-                {
-                    url = temp_filename;
-                }
-            }
-        }
-        url_size = strlen(url);
-        log("NEW URL: %s\n", url);
-
-        /* find file */
-        int fd = open(url, O_RDONLY);
-        size_t file_size = 0;
-
-        char *header_type = "HTTP/1.1 200 OK";
-        // check 404
-        if(fd < 0)
-        {
-            if(url == _file_error_404) {send_default_404_msg(client_fd); goto EXIT_REQUEST;}
-
-            url = _file_error_404;
-            fd = open(url, O_RDONLY);
-            if(fd < 0) {send_default_404_msg(client_fd); goto EXIT_REQUEST;}
-
-            file_size = file_get_size(url);
-            header_type = "HTTP/1.1 404 NOT FOUND";
-        }
-
-        /* send */
-        // send header
-        char send_buf[SEND_BUFFER_CAP] = {0};
-        size_t send_buf_offset = 0;
-        file_size = file_get_size(url);
-        send_buf_offset = snprintf(
-                send_buf, SEND_BUFFER_CAP,
-                "%s\r\n"
-                "Content-Type: %s\r\n"
-                "Content-Length: %ld\r\n"
-                "\r\n",
-                header_type,
-                mime_type_default(url),
-                file_size);
-        if(send_buf_offset > SEND_BUFFER_CAP) {die("failed to write header");}
-        socket_send_all(client_fd, send_buf, send_buf_offset);
-
-        // send file
-        if(sendfile(client_fd, fd, NULL, file_size) < 0)
-        {
-            diep("failed to send file");
-        }
-        close(fd);
-
-EXIT_REQUEST:
-        close(client_fd);
+        thread_data_arr[thread_to_add_to].client = client_fd;
+        sem_post(&thread_data_arr[thread_to_add_to].notify);
     }
 
-    globfree(&blacklisted);
+    globfree(&_blacklisted);
 
     return(0);
 }
